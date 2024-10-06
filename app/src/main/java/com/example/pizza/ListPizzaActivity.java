38f9f80457b87c199d9cc426caf00f649abcdd2e
chouay walid <walidchouaye2020@gmail.com>
2024-10-06 22:48:03 +0100

    secoond commit

--- app/src/main/java/com/example/pizza/ListPizzaActivity.java
+++ app/src/main/java/com/example/pizza/ListPizzaActivity.java
@@ -65,5 +65,6 @@ public class ListPizzaActivity extends AppCompatActivity {
         });
 
 
+
     }
 }
--- app/src/main/java/com/example/pizza/adapter/ProduitAdapter.java
+++ app/src/main/java/com/example/pizza/adapter/ProduitAdapter.java
@@ -2,12 +2,14 @@ package com.example.pizza.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.pizza.R;
 import com.example.pizza.beans.Produit;
@@ -18,10 +20,12 @@ public class ProduitAdapter  extends BaseAdapter {
 
     private List<Produit> produits;
     private LayoutInflater inflater;
+    private Context context;
 
     public ProduitAdapter(List<Produit> produits, Activity activity) {
         this.produits = produits;
         this.inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.context = activity;
     }
 
     @Override
@@ -48,6 +52,8 @@ public class ProduitAdapter  extends BaseAdapter {
         ImageView photo = convertView.findViewById(R.id.photo);
         TextView id = convertView.findViewById(R.id.id);
         TextView nbr = convertView.findViewById(R.id.nbr);
+        ImageView partage = convertView.findViewById(R.id.partage);
+        final Produit produit = produits.get(position);
 
 
 
@@ -58,6 +64,33 @@ public class ProduitAdapter  extends BaseAdapter {
         nbr.setText(produits.get(position).getNbrIngredient()+"");
 
 
+        partage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareViaWhatsApp(produit);
+            }
+        });
+
         return convertView;
     }
+    private void shareViaWhatsApp(Produit produit) {
+        Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
+        whatsappIntent.setType("text/plain");
+        whatsappIntent.setPackage("com.whatsapp");
+
+        String message = "Check out this product: " + produit.getNom() +
+                "\nDuration: " + produit.getDuree() +
+                "\nIngredients: " + produit.getNbrIngredient();
+
+        whatsappIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        try {
+            context.startActivity(whatsappIntent);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(context, "WhatsApp is not installed on this device.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 }
